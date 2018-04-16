62783460b149e1811ea8132d787a36609b7b3829
jackie <jcqnly@gmail.com>
2018-04-15 17:47:12 -0700

    complete steps through 7

---
+++ scripts/models/task.js
@@ -0,0 +1,43 @@
+'use strict'
+console.log('entering tasks.js');
+var app = app || {};
+
+const ENV = {};
+
+ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'https://pk-jl-booklist.herokuapp.com';
+ENV.developmentApiUrl = 'https://localhost:3000';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+(function(module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function Book(bookData) {
+    console.log('entering Book function', bookData);
+
+    Object.keys(bookData).forEach(key => this[key] = bookData[key]);
+  }
+
+  Book.all = [];
+
+  Book.prototype.toHtml=function(){
+    var template = Handlebars.compile($('#book-template').text());
+    return template(this);
+  };
+
+  Book.loadAll = rows => {
+    Book.all = rows.sort((a,b) => b.title - a.title).map(book => new Book(book));
+  }
+
+  Book.fetchAll = callback =>
+    $.get(`${ENV.apiUrl}/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+
+  module.Book = Book;
+
+}(app));
---
+++ scripts/views/book-view.js
@@ -0,0 +1,18 @@
+'use strict'
+console.log('entering book-view.js');
+var app = app || {};
+(function (module) {
+  let bookView = [];
+
+  bookView.initIndexPage = function() {
+    $('.container').hide();
+    $('.book-view').show();
+    app.Book.all.map(book => $('#booklist').append(book.toHtml()));
+  }
+  module.bookView = bookView;
+})(app)
+
+$(function () {
+  console.log('IIFE working');
+  app.Book.fetchAll(app.bookView.initIndexPage);
+})
